bd30b690314adb47cd4a5562a698ed701a3c7b32
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2024-04-30 22:04:00 +0300

    777

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,29 +1,14 @@
- //import React, { useState } from 'react';
 import css from './HeroSection.module.css'; 
 import { useTranslation } from 'react-i18next';
-// import hornSound from '../../Sound/new_message_tone-1.mp3';
 
 const HeroSection = () => {
   const { t } = useTranslation();
-  // const [audio] = useState(new Audio(hornSound));
-  // const handleButtonClick = () => {
-  //   audio.play();
-  // };
+  
 
   return (
     <div className={css.heroSection}>
       <div className={css.heroOverlay}>
         <h1 className={css.heroTitle}>{t('Welcome')}<span className={css.rentaYellow}>{t('RENTA')}</span>{t('CARS')}</h1>
-        {/* <div className={css.steeringWheel}>
-          <button className={css.signal} onClick={handleButtonClick}></button>
-          <div className={css.putMe}>
-    <p>{t('p')}</p>
-    <p>{t('u')}</p>
-    <p>{t('t')}</p>
-    <p>{t('m')}</p>
-    <p>{t('e')}</p>
-  </div>
-        </div> */}
     </div>
     </div>
   );
