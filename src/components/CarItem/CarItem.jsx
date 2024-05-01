12e149784335b847c333ef608ac0a60c6e4592f5
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2024-05-01 13:04:06 +0300

    3

--- src/components/CarItem/CarItem.jsx
+++ src/components/CarItem/CarItem.jsx
@@ -1,4 +1,4 @@
-
+import HeartIcon from './HeartIcon.jsx';
 import functionSearchDetails  from '../HelpersFunction/functionSearchDetails';
 import phrases from 'components/HelpersFunction/phrases';
 import React, { useState, useEffect } from 'react';
@@ -65,10 +65,8 @@ const CarItem = ({ car, openModal }) => {
     <div className={css.carItem}>
       <img className={css.imageItem} src={car.img} alt={`${car.make} ${car.model}`} />
       <button onClick={handleClick} onTouchStart={handleTouchStart} className={css.heart}>
-  <span style={{ fill: isFavorite ? '#ff0000' : '#ffffff' }}>
-    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
-      <path d="M10 18.35l-1.45-1.32C4.4 13.36 1 10.25 1 6.5 1 3.42 3.42 1 6.5 1c1.74 0 3.41.81 4.5 2.09C11.09 1.81 12.76 1 14.5 1 17.58 1 20 3.42 20 6.5c0 3.75-3.4 6.86-8.55 10.54L10 18.35z"/>
-    </svg>
+  <span>
+  <HeartIcon fill={isFavorite ? '#ff0000' : '#ffffff'} />
   </span>
 </button>
       {/* <span className={css.heartIconUnder}>
