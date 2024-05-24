5db7da3dae8e3e8c4aadebedc4468cbc3608ecd7
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2024-05-24 07:30:25 +0300

    1

--- src/components/LanguageSelector/LanguageSelector.jsx
+++ src/components/LanguageSelector/LanguageSelector.jsx
@@ -1,20 +1,63 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+// import ReactFlagsSelect from 'react-flags-select';
+// import { useTranslation } from 'react-i18next';
+
+// const LanguageSelector = () => {
+//   const { i18n } = useTranslation();
+//   const [selected, setSelected] = useState("UA");
+  
+//   const handleChangeLanguage = (code) => {
+//     i18n.changeLanguage(code);
+//     setSelected(code);
+//   };
+//   return (
+//     <div >
+//       <ReactFlagsSelect 
+//         countries={['US', 'RU', 'UA']}
+//         selectedSize={24} 
+//         selected={selected}
+//         showSelectedLabel={false}
+//         customLabels={{ US: "en", RU: "ru", UA: "uk" }}
+//         onSelect={handleChangeLanguage}
+//       />
+//     </div>
+//   );
+// };
+
+// export default LanguageSelector;
+
+import React, { useState, useEffect } from "react";
 import ReactFlagsSelect from 'react-flags-select';
 import { useTranslation } from 'react-i18next';
-// import css from './LanguageSelector.module.css'
+
 const LanguageSelector = () => {
   const { i18n } = useTranslation();
   const [selected, setSelected] = useState("UA");
-  
+
+  useEffect(() => {
+    const currentLanguage = i18n.language;
+    if (currentLanguage.startsWith('en')) {
+      setSelected('US');
+    } else if (currentLanguage.startsWith('ru')) {
+      setSelected('RU');
+    } else if (currentLanguage.startsWith('uk')) {
+      setSelected('UA');
+    } else {
+      i18n.changeLanguage('uk');
+      setSelected('UA');
+    }
+  }, [i18n]);
+
   const handleChangeLanguage = (code) => {
-    i18n.changeLanguage(code);
-    setSelected(code);
-  };
+        i18n.changeLanguage(code);
+        setSelected(code);
+      };
+
   return (
-    <div >
+    <div>
       <ReactFlagsSelect 
         countries={['US', 'RU', 'UA']}
-        selectedSize={24} 
+        selectedSize={24}
         selected={selected}
         showSelectedLabel={false}
         customLabels={{ US: "en", RU: "ru", UA: "uk" }}
