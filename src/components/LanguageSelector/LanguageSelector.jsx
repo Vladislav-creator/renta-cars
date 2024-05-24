7f007cf6af512b8117a9f27d079ea88e12220e88
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2024-05-24 15:09:56 +0300

    flag

--- src/components/LanguageSelector/LanguageSelector.jsx
+++ src/components/LanguageSelector/LanguageSelector.jsx
@@ -1,31 +1,3 @@
-// import React, { useState } from "react";
-// import ReactFlagsSelect from 'react-flags-select';
-// import { useTranslation } from 'react-i18next';
-
-// const LanguageSelector = () => {
-//   const { i18n } = useTranslation();
-//   const [selected, setSelected] = useState("UA");
-  
-//   const handleChangeLanguage = (code) => {
-//     i18n.changeLanguage(code);
-//     setSelected(code);
-//   };
-//   return (
-//     <div >
-//       <ReactFlagsSelect 
-//         countries={['US', 'RU', 'UA']}
-//         selectedSize={24} 
-//         selected={selected}
-//         showSelectedLabel={false}
-//         customLabels={{ US: "en", RU: "ru", UA: "uk" }}
-//         onSelect={handleChangeLanguage}
-//       />
-//     </div>
-//   );
-// };
-
-// export default LanguageSelector;
-
 import React, { useState, useEffect } from "react";
 import ReactFlagsSelect from 'react-flags-select';
 import { useTranslation } from 'react-i18next';
@@ -54,7 +26,7 @@ const LanguageSelector = () => {
       };
 
   return (
-    <div>
+    <div >
       <ReactFlagsSelect 
         countries={['US', 'RU', 'UA']}
         selectedSize={24}
